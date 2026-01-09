456202db36f411eb6804441a83ce58951b0e1f93
hoangnghia11 <htnghiaxx@gmail.com>
2026-01-09 17:49:10 +0700

    update

--- webapp/channels/src/actions/views/posts.js
+++ webapp/channels/src/actions/views/posts.js
@@ -1,37 +1,10 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
-import { logError } from 'mattermost-redux/actions/errors';
-import * as PostActions from 'mattermost-redux/actions/posts';
-import { Permissions } from 'mattermost-redux/constants';
-import { getLicense } from 'mattermost-redux/selectors/entities/general';
-import { getAssociatedGroupsForReferenceByMention } from 'mattermost-redux/selectors/entities/groups';
-import { isCustomGroupsEnabled } from 'mattermost-redux/selectors/entities/preferences';
-import { haveIChannelPermission, haveICurrentChannelPermission } from 'mattermost-redux/selectors/entities/roles';
-import { getCurrentTeam } from 'mattermost-redux/selectors/entities/teams';
-import { getCurrentUserId } from 'mattermost-redux/selectors/entities/users';
+import { getChannel } from 'mattermost-redux/selectors/entities/channels';
+import { General } from 'mattermost-redux/constants';
 
-import { getPermalinkURL } from 'selectors/urls';
-
-import { ActionTypes, AnnouncementBarTypes } from 'utils/constants';
-import { containsAtChannel, groupsMentionedInText } from 'utils/post_utils';
-import { getSiteURL } from 'utils/url';
-import { getTimestamp } from 'utils/utils';
-
-import { runMessageWillBePostedHooks } from '../hooks';
-
-export function editPost(post) {
-    return async (dispatch) => {
-        const result = await dispatch(PostActions.editPost(post));
-
-        // Send to error bar if it's an edit post error about time limit.
-        if (result.error && result.error.server_error_id === 'api.post.update_post.permissions_time_limit.app_error') {
-            dispatch(logError({ type: AnnouncementBarTypes.ANNOUNCEMENT, message: result.error.message }, true));
-        }
-
-        return result;
-    };
-}
+// ... existing code ...
 
 export function forwardPost(post, channel, message = '') {
     return async (dispatch, getState) => {
@@ -45,41 +18,41 @@ export function forwardPost(post, channel, message = '') {
         const permaLink = `${getSiteURL()}${relativePermaLink}`;
 
         const license = getLicense(state);
-        const isLDAPEnabled = license?.IsLicensed === 'true' && license?.LDAPGroups === 'true';
-        const useLDAPGroupMentions = isLDAPEnabled && haveICurrentChannelPermission(state, Permissions.USE_GROUP_MENTIONS);
-        const useChannelMentions = haveIChannelPermission(state, channel.team_id, channelId, Permissions.USE_CHANNEL_MENTIONS);
-        const useCustomGroupMentions = isCustomGroupsEnabled(state) && haveICurrentChannelPermission(state, Permissions.USE_GROUP_MENTIONS);
-        const groupsWithAllowReference = useLDAPGroupMentions || useCustomGroupMentions ? getAssociatedGroupsForReferenceByMention(state, currentTeam.id, channelId) : null;
+        // ... permission checks ...
 
         let newPost = {};
-
         newPost.channel_id = channelId;
 
         const time = getTimestamp();
         const userId = currentUserId;
 
+        // Determine if source is private
+        const sourceChannel = getChannel(state, post.channel_id);
+        const isSourcePrivate = sourceChannel && (sourceChannel.type === General.PRIVATE_CHANNEL || sourceChannel.type === General.DM_CHANNEL || sourceChannel.type === General.GM_CHANNEL);
+
         let quote = '';
         if (post.message) {
             quote = `> ${post.message.replace(/\n/g, '\n> ')}\n\n`;
         }
-        newPost.message = `${quote}${message ? message + '\n' : ''}${permaLink}`;
 
+        // Append permalink ONLY if source is NOT private
+        const linkToAppend = isSourcePrivate ? '' : permaLink;
+        newPost.message = `${quote}${message ? message + '\n' : ''}${linkToAppend}`;
+
+        // Handle files: fetch from state to ensure createPost has them
+        let files = [];
         if (post.file_ids && post.file_ids.length > 0) {
             newPost.file_ids = post.file_ids;
+            files = post.file_ids.map((id) => state.entities.files.files[id]).filter(f => f);
         }
+
         newPost.pending_post_id = `${userId}:${time}`;
         newPost.user_id = userId;
         newPost.create_at = time;
         newPost.metadata = {};
         newPost.props = {};
 
-        if (!useChannelMentions && containsAtChannel(newPost.message, { checkAllMentions: true })) {
-            newPost.props.mentionHighlightDisabled = true;
-        }
-
-        if (!useLDAPGroupMentions && !useCustomGroupMentions && groupsMentionedInText(newPost.message, groupsWithAllowReference)) {
-            newPost.props.disable_group_highlight = true;
-        }
+        // ... hooks and permission checks ...
 
         const hookResult = await dispatch(runMessageWillBePostedHooks(newPost));
 
@@ -89,7 +62,7 @@ export function forwardPost(post, channel, message = '') {
 
         newPost = hookResult.data;
 
-        return dispatch(PostActions.createPost(newPost, []));
+        return dispatch(PostActions.createPost(newPost, files));
     };
 }
 
